d6858f3b13478eae0bf73385762d7abbb0eb16b8
SuhunaK <83697679+SuhunaK@users.noreply.github.com>
2021-10-13 12:59:40 +0530

    add user model and connect to db

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
+const User=require('./models/User');
+mongoose.connect('mongodb://localhost/userData')
+
 const port=8000;
 const app= express();
+
 
 app.use(bodyParser.json());
 
